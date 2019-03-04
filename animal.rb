7d998839ff13266614ebefbc86a96da628f527d8
YuchiNagai <alone.on.the.rope1231@gmail.com>
2019-03-04 16:57:35 +0900

    課題2-5完成

--- animal.rb
+++ animal.rb
@@ -3,10 +3,23 @@ class Animal
 
   def initialize(name)
     @name = name
-    @voice = '...'
   end
 
   def cry
-    "#{self.name}: #{voice}"
+    "#{self.name}: #{self.voice}"
+  end
+end
+
+class Dog < Animal
+  def initialize(name)
+    super
+    @voice = "ワンワン！"
+  end
+end
+
+class Cat < Animal
+  def initialize(name)
+    super
+    @voice = "ニャー！"
   end
 end
